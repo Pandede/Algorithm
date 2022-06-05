cc1dbfff79e87f7330d8563bbecab8055ca7ab7c
Chan, Steven <steven.chan@garmin.com>
2022-06-05 17:26:53 +0800

    add exceptions

--- SeamCarving/SeamCarver.java
+++ SeamCarving/SeamCarver.java
@@ -34,7 +34,13 @@ public class SeamCarver {
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
-        if (isBorder(x, y))
+        if (x < 0 || x >= width() || y < 0 || y >= height())
+            throw new IllegalArgumentException();
+
+        if (energyGrid[y][x] != 0)
+            return energyGrid[y][x];
+
+        if (x == 0 || x == width() - 1 || y == 0 || y == height() - 1)
             return 1000.0;
 
         Color left = picture.get(x - 1, y);
@@ -55,23 +61,18 @@ public class SeamCarver {
         return (double) (dr * dr + dg * dg + db * db);
     }
 
-    private boolean isBorder(int x, int y) {
-        return x == 0 || x == width() - 1 || y == 0 || y == height() - 1;
-    }
-
     private void relaxEdge(
-            int fromCol, int fromRow, int toCol, int toRow,
-            double[][] distTo, int[][] edgeTo, double[][] energyGrid) {
-        double nextEnergy = distTo[fromRow][fromCol] + energyGrid[toRow][toCol];
+            int fromCol, int fromRow, int toCol, int toRow, double[][] distTo, int[][] edgeTo, double[][] grid) {
+        double nextEnergy = distTo[fromRow][fromCol] + grid[toRow][toCol];
         if (nextEnergy < distTo[toRow][toCol]) {
             distTo[toRow][toCol] = nextEnergy;
             edgeTo[toRow][toCol] = fromRow;
         }
     }
 
-    private int[] findSeam(double[][] energyGrid) {
-        int height = energyGrid.length;
-        int width = energyGrid[0].length;
+    private int[] findSeam(double[][] grid) {
+        int height = grid.length;
+        int width = grid[0].length;
         double[][] distTo = new double[height][width];
         int[][] edgeTo = new int[height][width];
 
@@ -139,6 +140,12 @@ public class SeamCarver {
 
     // remove horizontal seam from current picture
     public void removeHorizontalSeam(int[] seam) {
+        if (seam == null)
+            throw new IllegalArgumentException();
+
+        if (width() <= 1)
+            throw new IllegalArgumentException();
+
         Picture updatedPicture = new Picture(width(), height() - 1);
         double[][] updatedEnergy = new double[height() - 1][width()];
         for (int col = 0; col < width(); col++) {
@@ -157,6 +164,12 @@ public class SeamCarver {
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
+        if (seam == null)
+            throw new IllegalArgumentException();
+
+        if (height() <= 1)
+            throw new IllegalArgumentException();
+
         Picture updatedPicture = new Picture(width() - 1, height());
         double[][] updatedEnergy = new double[height()][width() - 1];
         for (int row = 0; row < height(); row++) {
@@ -176,6 +189,61 @@ public class SeamCarver {
 
     // unit testing (optional)
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Test for exceptions
+        Picture picture = new Picture("./seam/12x10.png");
+        SeamCarver carver = new SeamCarver(picture);
+
+        int width = carver.width();
+        int height = carver.height();
+
+        // 1. Retrieve energy out of the picture
+        try {
+            carver.energy(width + 1, height);
+            carver.energy(width, height + 1);
+            carver.energy(-1, height);
+            carver.energy(width, -1);
+            carver.energy(width + 1, height + 1);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[EnergyOutOfPicture] passed");
+        }
+
+        // 2. removeHorizontalSeam with null argument
+        try {
+            carver.removeHorizontalSeam(null);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[RemoveNullHorizontalSeam] passed");
+        }
+
+        // 3. removeVerticalSeam with null argument
+        try {
+            carver.removeVerticalSeam(null);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[RemoveNullVerticalSeam] passed");
+        }
+
+        // 4. Remove seam with less than or equals than width or height of 1
+        try {
+            int[] seam;
+            // 4.1 With width of 1
+            picture = new Picture("./seam/1x8.png");
+            carver = new SeamCarver(picture);
+            seam = carver.findHorizontalSeam();
+            carver.removeHorizontalSeam(seam);
+
+            // 4.2 With height of 1
+            picture = new Picture("./seam/8x1.png");
+            carver = new SeamCarver(picture);
+            seam = carver.findVerticalSeam();
+            carver.removeVerticalSeam(seam);
+
+            // 4.3 With single pixel
+            picture = new Picture("./seam/1x1.png");
+            carver = new SeamCarver(picture);
+            seam = carver.findVerticalSeam();
+            carver.removeVerticalSeam(seam);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[RemoveSeamWithSingleLine] passed");
+        }
+
     }
 }
